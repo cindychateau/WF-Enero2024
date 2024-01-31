65b7414ba9fc11d1a2dd7b6d9b7f82edb608f13b
Cynthia Castillo <ca.castillo@gmail.com>
2024-01-30 19:27:37 -0600

    DOM+Query Selector

--- 4.- Javascript/profile-page/js/script.js
+++ 4.- Javascript/profile-page/js/script.js
@@ -47,3 +47,38 @@ function cambiar_color(elemento_parr) {
     //margin-left -> marginLeft
     elemento_parr.style.marginLeft = "40px";
 }
+
+function editar_perfil() {
+    var elemento_nombre = document.querySelector("#nombre");
+    elemento_nombre.innerText = "Elena de Troya";
+
+    var elemento_locacion = document.querySelector("h3 span");
+    elemento_locacion.innerText = "Roma";
+
+    var elemento_p = document.querySelector('.descripcion');
+    elemento_p.innerHTML = "Desarrolladora, Ingeniera <br> Reina emperatriz de Troya";
+
+    //Lista/Arreglo con todas las clases
+    //elemento_nombre.classList.add("h1_darkmode");
+    if(elemento_nombre.classList.contains("h1_darkmode")) {
+        //Quitamos la clase
+        elemento_nombre.classList.remove("h1_darkmode");
+    } else {
+        //Agregamos la clase
+        elemento_nombre.classList.add("h1_darkmode");
+    }
+}
+
+var click_arriba = 0;
+var click_abajo = 0;
+
+function hizo_click_arriba() {
+    click_arriba++;
+    var elemento_numero = document.querySelector('#numero-arriba');
+    elemento_numero.innerText = click_arriba;
+}
+
+function hizo_click_abajo() {
+    click_abajo++;
+    var elemento_numero = document.querySelector('#numero-abajo');elemento_numero.innerText = click_abajo;
+}
